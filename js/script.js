205e3f033b976c7c180594d5a0917a6ea5a621e4
Tarundeep <manavtarundeep2001@gmail.com>
2023-08-16 18:21:41 +0530

    product menu fix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const products_btn= document.querySelector('#pro');
 const products_menu= document.querySelector('#product-box');
 const nav_section= document.querySelector('#nav-section');
+const nav_bar= document.querySelector('#nav-bar');
+const drop_down= document.querySelector('#drop-down');
 
 products_menu.remove();
 var menuOpen= 1;
 
 products_btn.addEventListener('click',openMenu);
+
 function openMenu(){
     if((menuOpen%2==1)){
-        nav_section.appendChild(products_menu);
+        drop_down.appendChild(products_menu);
     }
     else{
         close();
@@ -27,6 +30,3 @@ function closeMenu(event) {
 }
 
 document.addEventListener('click', closeMenu, false);
-
-
-
